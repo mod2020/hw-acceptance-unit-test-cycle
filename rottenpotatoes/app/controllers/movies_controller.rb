443bebbc3aa6c0fbcf9c004721a03b962fe508ad
EC2 Default User <ec2-user@ip-172-31-23-159.us-east-2.compute.internal>
2021-10-13 01:17:38 +0000

    working on part2

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,13 +1,49 @@
 class MoviesController < ApplicationController
 
+
+  #HW Part 1#
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
+  
+  def similar
+    movie_title = Movie.find(params[:id]).title
+    @movies = Movie.with_director(movie_title)
+    if @movies.nil? || @movies == ""
+      flash[:notice] = "'#{movie_title}' has no director info."
+      redirect_to movies_path
+    end
+    
 
-  def index
-    @movies = Movie.all
+  end
+  
+  def index #index.html
+    #@movies = Movie.all
+    ######added
+    @all_ratings = Movie.all_ratings #
+    @ratings_to_show = params[:ratings] || {} 
+    ratings_list = @ratings_to_show
+    session[:ratings]= @ratings_to_show #part3
+    if @ratings_to_show == {}
+      ratings_list = Hash[@all_ratings.map {|x| [x, 1]}] #assign any value
+    end
+    
+    #update movies filtered by ratings
+    
+    @movies = Movie.with_ratings(ratings_list.keys)
+    ######
+    @clicked_header = params[:clicked_header] || "" #session[:clicked_header] || ""
+    session[:clicked_header] = @clicked_header #part3
+    #sort movies in order
+    if @clicked_header == "title_header"
+      @movies = @movies.order(:title)
+    end
+    if @clicked_header == "release_date_header"
+      @movies = @movies.order(:release_date)
+    end
+    
   end
 
   def new
@@ -42,6 +78,6 @@ class MoviesController < ApplicationController
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 end
